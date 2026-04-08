4d76a63fa2dcbba82701159d7ff676cfb9acc12d
lauraxime589 <150557859+lauraxime589@users.noreply.github.com>
2026-04-08 14:04:57 -0500

    feat: Agregar clase Zona con constructores y método para generar ID

---
+++ src/Zona.java
@@ -0,0 +1,26 @@
+public class Zona {
+    private String id;
+    private String nombre;
+    private int capacidadMaxima;
+    private int visitantesActuales;
+    private ListaEnlazada<Atraccion> atracciones;
+    private ListaEnlazada<Operador> operadores;
+    // Constructor
+    public Zona(String id, String nombre, int capacidadMaxima) {
+        this.id = id;
+        this.nombre = nombre;
+        this.capacidadMaxima = capacidadMaxima;
+        this.visitantesActuales = 0;
+        this.atracciones = new ListaEnlazada<>();
+        this.operadores = new ListaEnlazada<>();
+    }
+   // Constructor adicional para crear una zona con un ID generado automáticamente y una capacidad predeterminada
+    public Zona(String nombre) {
+        this(generarId(), nombre, 100);
+    }
+
+    private static String generarId() {
+        return "ZONA_" + System.currentTimeMillis();
+    }
+/
+
