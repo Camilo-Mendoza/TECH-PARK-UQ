bb0be328d75d4bc860b55ed8f1fe5e1fc8f80b6c
lauraxime589 <150557859+lauraxime589@users.noreply.github.com>
2026-04-09 15:14:35 -0500

    feat: Agregar clase Reporte para gestionar reportes diarios del parque

---
+++ src/Reporte.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Clase para representar un reporte diario del parque de diversiones
+public class Reporte {
+    private LocalDate fecha;
+    private double ingresosDiarios;
+    private List<Atraccion> atraccionesMasVisitadas;
+    private Map<String, Double> tiemposPromedioEspera;
+    private int cierresPorClima;
+    private int alertasMantenimiento;
+    private List<Atraccion> atraccionesConIncidentes;
+// Constructor por defecto para inicializar el reporte con valores predeterminados
+    public Reporte() {
+        this.fecha = LocalDate.now();
+        this.ingresosDiarios = 0.0;
+        this.atraccionesMasVisitadas = new ArrayList<>();
+        this.tiemposPromedioEspera = new HashMap<>();
+        this.cierresPorClima = 0;
+        this.alertasMantenimiento = 0;
+        this.atraccionesConIncidentes = new ArrayList<>();
+    }
+
+}
+
